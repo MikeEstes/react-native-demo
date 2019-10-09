8f26f20171fdf4934e5c9e129d914f208002913f
Mike Estes <sregge@gmail.com>
2019-10-09 17:10:42 -0400

    Pushing Files

--- app/config/router.js
+++ app/config/router.js
@@ -1,19 +1,32 @@
-import { StackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
 
-export const ContactsStack = StackNavigator({
+const ContactsStack = createStackNavigator({
+  Contacts: {
+    screen: Contacts
+  },
+  Details: {
+    screen: Details
+  }
+});
+
+export default createAppContainer(ContactsStack);
+
+/*export const ContactsStack = StackNavigator({
   Contacts: {
     screen: Contacts,
     navigationOptions: {
       title: 'Contacts'
     }
-  },
+  }
   Details: {
     screen: Details,
     navigationOptions: {
       title: 'Details'
     }
   }
-});
+});*/
--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { FlatList } from 'react-native';
+
+import { contacts } from '../config/data';
+import colors from '../config/colors';
+import { ListItem } from '../components/ListItem';
 
 class Contacts extends Component {
-    render() {
-        return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} >
-              <Text>Contacts Screen</Text>
-          </View>  
-        );
-    }
+  handleRowPress = item => {
+    this.props.navigation.navigate('Details', item);
+  };
+
+  render() {
+    return (
+      <FlatList
+        style={{ backgroundColor: colors.background }}
+        data={contacts}
+        renderItem={({ item }) => (
+          <ListItem contact={item} onPress={() => this.handleRowPress(item)} />
+        )}
+        keyExtractor={item => item.email}
+      />
+    );
+  }
 }
 export default Contacts;
